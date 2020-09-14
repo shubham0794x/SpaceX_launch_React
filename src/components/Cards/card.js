f0957461f6e23b31e909e380c9fc54fe8e19f684
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-09-14 18:01:37 +0530

    Update card.js

--- src/components/Cards/card.js
+++ src/components/Cards/card.js
@@ -10,6 +10,8 @@ import {
   EventDetails,
   DetailValue,
 } from "../../styling";
+import defaultImg from "../../assets/default.png";
+
 
 const Card = (props) => {
   const {
@@ -40,7 +42,7 @@ const Card = (props) => {
        <EventLogo>
         <EventImg
           data-testid="logo"
-          src={mission_patch_small}
+          src={mission_patch_small ? mission_patch_small : defaultImg}
           alt=""
         />
       </EventLogo>
